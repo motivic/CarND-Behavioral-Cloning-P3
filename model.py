1eaf90508ed9dcc9503bf95439d437450d699c0d
Johnson Jia [ubuntu] <johnson.jia@live.com>
2017-03-04 19:50:16 -0500

    adding frames flipped horizontally

--- model.py
+++ model.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from itertools import chain
+from keras.models import Model
+from keras.layers import Input, Dense, Flatten, Activation
+import matplotlib.pyplot as plt
 import numpy as np
 import multiprocessing
 import os
-import pandas as pd
 import pathlib
 from PIL import Image
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
 import timeit
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+# Command line flags
+flags.DEFINE_integer('batch_size', 128, 'Batch size')
+flags.DEFINE_integer('epochs', 10, 'Number of epochs')
 
 def parse_log_entry(log_entry):
     """ Return the image frames in the log as a list of numpy arrays
@@ -29,41 +40,17 @@ def parse_log_entry(log_entry):
         frames.append(np.asarray(image))
     return frames, [np.float32(l[3])]*3
 
-def read_images(*paths):
-    """ Read image frames into a 4-dimensional numpy array.
-
-    Args:
-        *paths: An iterable of such diretories.
-
-    Returns:
-        A 4-dimensional numpy array consisting of the images.
-    """
-    start_time = timeit.default_timer()
-    pool = multiprocessing.Pool()
-    images = []
-    if isinstance(paths, str):
-        paths = [paths]
-    for p in paths:
-        path = pathlib.Path(p)
-        images.extend(pool.map(worker, path.iterdir()))
-    print("Time elapsed for reading image files: {:.2f} seconds"\
-          .format(timeit.default_timer() - start_time))
-    return np.array(images)
-
-
 def parse_driving_log(*paths):
-    """ Return a 4-dim numpy array of the image frames and a numpy
-    array of the corresponding steering angles read from the logs in `paths`.
+    """ Return a list of the image frames and a list of the
+    corresponding steering angles read from the logs in `paths`.
 
     Args:
         *paths: A driving log file or an iterable of driving log files
             containing the steering angles.
 
     Returns:
-        A 4-dim numpy array consisting of the images and a numpy array
-        of the steering angles.
+        A list consisting of the images and a list of the steering angles.
     """
-    start_time = timeit.default_timer()
     pool = multiprocessing.Pool()
     images = []
     steering_angles = []
@@ -77,12 +64,62 @@ def parse_driving_log(*paths):
             steering_angles.extend(r[1] for r in results)
     images = list(chain.from_iterable(images))
     steering_angles = list(chain.from_iterable(steering_angles))
-    print("Time elapsed for parsing driving logs: {:.2f} seconds" \
-          .format(timeit.default_timer() - start_time))
-    return np.array(images), np.array(steering_angles)
+    return images, steering_angles
+
+
+def flip_images(images):
+    """
+
+    Args:
+        images:
+
+    Returns:
+
+    """
+    pass
+
+
+def trim_imgaes(images):
+    """
+
+    Args:
+        images:
+
+    Returns:
+
+    """
+    pass
+
+
+def main(_):
+    # Read in images (X) and steering angles (y)
+    images = []
+    steering_angles = []
+    HOME = os.getenv('HOME')
+    path = pathlib.Path(HOME + '/data')
+    for folder in path.iterdir():
+        start_time = timeit.default_timer()
+        if not folder.match(r'driving*'):
+            continue
+        file = folder.joinpath('driving_log.csv').as_posix()
+        imgs, angles = parse_driving_log(file)
+        print("Time elapsed for parsing {log}: {elapse:.2f} seconds" \
+              .format(log=file, elapse=timeit.default_timer() - start_time))
+        images.extend(imgs)
+        steering_angles.extend(angles)
+    X = np.array(images)
+    y = np.array(steering_angles)
+
+    # Preprocessing
+    X_flipped = X[:,:,::-1,:]
+    img = Image.fromarray(X_flipped[0])
+    img.save('flipped.jpg')
+    img = Image.fromarray(X[0])
+    img.save('original.jpg')
+    X_train, X_val, y_train, y_val = train_test_split(X, y)
+
+    # Train ConvNet model
 
 
 if __name__ == '__main__':
-    HOME = os.getenv('HOME')
-    LOG1 = HOME + '/data/driving_sim_track1_1/driving_log.csv'
-    images, steering_angles = parse_driving_log(LOG1)
+    tf.app.run()
